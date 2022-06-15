f03c559aeb9afd857e1a04f1b6dc5fb46b87181b
GTANAKA-LAB <91655832+GTANAKA-LAB@users.noreply.github.com>
2022-06-15 14:36:40 +0900

    Add files via upload

---
+++ Waveform/data/main.m
@@ -0,0 +1,7 @@
+% Copyright (c) 2022 Gouhei Tanaka. All rights reserved.
+% Citation: G.Tanaka and R.Nakane, Scientific Reports, 12, 9868 (2022).
+% DOI: 10.1038/s41598-022-13687-z
+
+function main()
+
+generateData()
